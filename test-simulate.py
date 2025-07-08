49e667689450bd0cf3fad0c90372c1cd95308724
AJM-Robotics <156541885+AJM-Robotics@users.noreply.github.com>
2025-07-08 12:29:20 -0400

    Update test-simulate.py

--- test-simulate.py
+++ test-simulate.py
@@ -1,6 +1,7 @@
 from robocasa.environments.kitchen.kitchen import Kitchen
 import mujoco
 from mujoco.viewer import launch
+import os
 
 env = Kitchen(
     layout_ids=8,
@@ -9,6 +10,11 @@ env = Kitchen(
 
 xml_str = env.sim.model.get_xml()
 model = mujoco.MjModel.from_xml_string(xml_str)
-data = mujoco.MjData(model)
+output_path = "dumped_kitchen.xml"
+mujoco.mj_saveLastXML(output_path, model)
+print(f"Model saved to: {os.path.abspath(output_path)}")
 
+'''
+data = mujoco.MjData(model)
 viewer = launch(model, data)
+'''
