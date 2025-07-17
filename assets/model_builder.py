8c5c556cbe9c5b9313673173804a4492742e4587
Saad A Ghani <saadhku2@gmail.com>
2025-07-17 13:56:27 -0400

    model_builder instructions

--- assets/model_builder.py
+++ assets/model_builder.py
@@ -1,1 +1,15 @@
 # this file is supposed to combine the different xml files in a similar way to robosuite
+
+
+"""
+1- This file will first load the config file: config/jackal_in_kitchen.yaml
+
+2- Depending on the robot, env and robot sensors, it will load up the different xml files from 
+    - environments/ 
+    - models/robots
+    - models/sensors
+
+3- place the sesnors on the robot, based on the config file.
+4- and then place the robot in the env, based on the config file.
+5- generates an xml file that has the sensors on the robot, the robot in the environment.
+"""
