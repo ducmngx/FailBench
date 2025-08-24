fb621f1796bf945d5abaa5306eceb24f1e91cad4
Saad A Ghani <saadhku2@gmail.com>
2025-08-23 21:21:15 -0400

    Collision Estimation complete. Need to write tester

--- failure_injection/collision_estimation.py
+++ failure_injection/collision_estimation.py
@@ -1,71 +1,147 @@
 import mujoco
 import numpy as np
+from failure_injection.collision_utils import *
 
 class CollisionEstimator:
 
-    def __init__(self, model, data, method_type="bounding_sphere", inflation_radius=0):
+    def __init__(self, model, data, method_type="bounding_sphere", inflation_radius=0, robot_root_name="link0"):
         self.model = model
         self.data = data
         self.method_type = method_type
         self.inflation_radius = inflation_radius
         self.collision_function = None
+        self.robot_root_name = robot_root_name
+        self._init_non_robot_geoms()
     
-    def estimate_collision_function(self, failing_joint, failure_type="aggressive"):
+    def _init_non_robot_geoms(self):
+        non_robot_bodies = get_non_robot_bodies(self.model, self.robot_root_name)
+        self.non_robot_geoms = get_bodies_geoms(self.model, non_robot_bodies)
+        return self.non_robot_geoms
+
+    def estimate_bodies_in_collision_with_multiple_failures(self, failing_joints, failure_type="aggressive"):
         """
-        The main method that will estimate the volume of collision when failure_type happens
-        at failing_joint.
-        failing_joint: the joint that fails (string)
-        failure_type: fixed to the only failure we have. string but can be enum.
+        The main method that will estimate the non-robot bodies that are estimated to be in collision upon failure_type failure at joint failing_joint.
+        failing_joint: the joint that fails. (string)
+        failure_type: fixed to the only failure we have, can be enum in the future. (string)
+        """
+        assert failure_type == "aggressive", "No other failure type is supported."
+
+        # get attached bodies 
+        joint_ids = [mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_JOINT, failing_joint) for failing_joint in failing_joints]
+        child_bids  = self.model.jnt_bodyid[joint_ids]
+        parent_bids = self.model.body_parentid[child_bids]
+        all_bids = np.unique(np.concatenate((child_bids, parent_bids)))
+
+        # get geoms in bodies
+        failing_geoms = get_bodies_geoms(self.model, all_bids) 
+
+        return self._estimate_bodies_in_collision(failing_geoms)
+        
+
+
+    def estimate_bodies_in_collision_with_single_failure(self, failing_joint, failure_type="aggressive"):
+        """
+        The main method that will estimate the non-robot bodies that are estimated to be in collision upon failure_type failure at joint failing_joint.
+        failing_joint: the joint that fails. (string)
+        failure_type: fixed to the only failure we have, can be enum in the future. (string)
         """
         assert failure_type == "aggressive", "No other failure type is supported."
 
         # get attached bodies 
         joint_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_JOINT, failing_joint)
-        child_bid  = mujoco.jnt_bodyid[joint_id]
-        parent_bid = mujoco.body_parentid[child_bid]
+        child_bid  = self.model.jnt_bodyid[joint_id]
+        parent_bid = self.model.body_parentid[child_bid]
 
         # get geoms in bodies
-        child_geoms = [mujoco.body_geomadr[addr] for addr in range(mujoco.body_geomadr[child_bid], mujoco.body_geomadr[child_bid] + mujoco.body_geomnum[child_bid])]
-        parent_geoms = [mujoco.body_geomadr[addr] for addr in range(mujoco.body_geomadr[parent_bid], mujoco.body_geomadr[parent_bid] + mujoco.body_geomnum[parent_bid])]
-        geoms_failing = child_geoms + parent_geoms
+        child_geoms = get_body_geoms(self.model, child_bid) 
+        parent_geoms = get_body_geoms(self.model, parent_bid) 
+        failing_geoms = np.concatenate([child_geoms, parent_geoms])
+
+        return self._estimate_bodies_in_collision(failing_geoms)
+
+    def _estimate_bodies_in_collision(self, failing_geoms):
+        """
+        Helper function for single and multiple failures.
+        """
+        # get robot-candidate geom pairs to check possible collision
+        colliding_id_pairs = self._get_colliding_geom_pairs(failing_geoms, self.non_robot_geoms)
 
         if self.method_type == "bounding_sphere":
-            self.collision_function = self._bounding_sphere_method(geoms_failing)
+            # check for collisions using bounding spheres - easier but a more course method.
+            estimated_collisions = self._bounding_sphere_method(colliding_id_pairs)
         elif self.method_type == "AABB":
-            raise NotImplementedError()
+            # check for collisions using AABB - not implemented but could be a faster and more finer method.
+            estimated_collisions = self._axis_aligned_bounding_box_method(colliding_id_pairs)
         else:
             raise NotImplementedError()
+
+        # given geom collision pairs, get the non-robot ones and return their body ids. 
+        estimated_collision_geom_ids = estimated_collisions[:, 1]
+        estimated_collision_body_ids = np.unique(self.model.geom_bodyid[estimated_collision_geom_ids])  # to get rid of duplicates
+        return estimated_collision_body_ids
+    
+
+
+    def _get_colliding_geom_pairs(self, robot_geoms, candidate_geoms):
+        """
+        Given robot_geoms and candidate_geoms, check against their contypes and conaffinities to filter out geoms that cannot collide against each other.
+        Useful to reduce the size of the problem for collision estimating.
+        """
+        non_robot_geom_contypes = self.model.geom_contype[candidate_geoms]
+        robot_geom_contypes = self.model.geom_contype[robot_geoms]
+
+        non_robot_geom_conaffinities = self.model.geom_conaffinity[candidate_geoms]
+        robot_geom_conaffinities = self.model.geom_conaffinity[robot_geoms]
         
-    def _bounding_sphere_method(self, geom_ids):
-        # get bounding sphere and world coordinates of geoms
-        geom_coords = [self.data.geom_xpos[geom_id] for geom_id in geom_ids]
-        geom_bounding_radius = [self.data.geom_rbound[geom_id] + self.inflation_radius for geom_id in geom_ids]
-        joint_sphere_data = [[x,y,z,r] for x,y,z,r in zip(geom_coords, geom_bounding_radius)]
+        robot_geom_length = len(robot_geoms)
+        contypes = np.concatenate((robot_geom_contypes, non_robot_geom_contypes))
+        conaffinities = np.concatenate((robot_geom_conaffinities, non_robot_geom_conaffinities))
 
-        # eqn of sphere: r^2 = (x-h)^2 + (y-k)^2 + (z-l)^2; (h,k,l) are coord center. if RHS < r^2, inside sphere. 
-        # eqn of cylinder: constraint: z_min <= z <= z_max, equation: r^2 = (x-x_0)^2 + (y-y_0)^2
+        first_test = contypes[:, None] & conaffinities[None, :] !=0 
+        second_test = conaffinities[:, None] & contypes[None, :] !=0
+        collision_test = first_test & second_test
+        np.fill_diagonal(collision_test, False)
+        lower_half_test = np.tril(collision_test)
+        candidate_idxs, robot_idxs = np.where(lower_half_test[robot_geom_length:, :robot_geom_length])
+        geom_id_pairs = np.stack((robot_geoms[robot_idxs], candidate_geoms[candidate_idxs]), axis=1)
+        return geom_id_pairs
 
-        # given the (x_i, y_i, z_i, r_i) of all the geom bounding spheres, we make the following system of linear equations:
-        # [(x-x_i)^2 + (y-y_i)^2 <= r_i^2] for i in range(len(geom_ids)) with the following constraint z <= z_i
-        def points_under_spheres(points):
-            """
-            Vectorized: given Nx3 points and M spheres, return boolean mask of length N.
-            """
-            points = np.asarray(points)
-            mask = np.zeros(len(points), dtype=bool)
-            for (xi, yi, zi, ri) in joint_sphere_data:
-                dx = points[:,0] - xi
-                dy = points[:,1] - yi
-                cond = (dx*dx + dy*dy <= ri*ri) & (points[:,2] <= zi)
-                mask |= cond
-            return mask
-        return points_under_spheres
+    def _bounding_sphere_method(self, geom_pairs):
+        """
+        Given (N,2) geom_pairs with the first column representing geoms of the failing bodies and the second column representing the candidate geoms that the failing geoms will collide with,
+        this method will return the pairs of geoms that will collide using bounding spheres.
+        Essentially, if a candidate geom's bounding sphere is below the failing geom's inflated bounding sphere, then the two are estimated to be in collision, given failure.
+        """
+
+        # get bounding sphere and world coordinates of robot geoms
+        robot_geom_coords = self.data.geom_xpos[geom_pairs[:,0]]
+        robot_geom_bounding_radius = self.model.geom_rbound[geom_pairs[:,0]] + self.inflation_radius
+
+        # get bounding sphere and world coordinates of candidate geoms
+        candidate_geom_coords = self.data.geom_xpos[geom_pairs[:,1]]
+        candidate_geom_bounding_radius = self.model.geom_rbound[geom_pairs[:,1]] 
+
+        # in collision if candidate geom's bounding sphere is under the bounding sphere of the robot's geom
+        dx = (candidate_geom_coords[:,0] -robot_geom_coords[:, 0])
+        dy = (candidate_geom_coords[:,1] -robot_geom_coords[:, 1])
+        cond = (dx**2 + dy**2 <= (robot_geom_bounding_radius + candidate_geom_bounding_radius)**2) & (candidate_geom_coords[:, 2] - candidate_geom_bounding_radius <= robot_geom_coords[:,2]) 
+        return geom_pairs[cond]
+
+    def _axis_aligned_bounding_box_method(self, geom_pairs):
+        raise NotImplementedError()
 
 
-    def _axis_aligned_bounding_box_method(self):
-        pass
+def test():
+    robot_xml_path = "/Users/saghani/Workspace/Research/GenAISim/franka_emika_panda/scene.xml"
+    model = mujoco.MjModel.from_xml_path(robot_xml_path)
+    data = mujoco.MjData(model)
 
-    def extract_bodies_in_collision(self):
-        assert self.cocollision_function is not None
+    mujoco.mj_forward(model, data)  # Update kinematics
 
-        
+    estimator = CollisionEstimator(model, data)
+    body_ids = estimator.estimate_bodies_in_collision_with_single_failure("joint1")
+    body_names = [mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_BODY, body_id) for body_id in body_ids]
+    print(body_ids, body_names)
+
+if __name__ == "__main__":
+    test()
---
+++ failure_injection/collision_utils.py
@@ -0,0 +1,45 @@
+import mujoco
+import numpy as np
+
+def get_body_descendants(model, root_bid):
+    """
+    Return all body IDs in the subtree rooted at root_bid.
+    Vectorized using NumPy (no inner Python loop over children).
+    """
+    parent = np.asarray(model.body_parentid)
+    descendants = set([root_bid])
+    frontier = {root_bid}
+
+    while frontier:
+        # For all bodies whose parent is in frontier
+        mask = np.isin(parent, list(frontier))
+        children = set(np.nonzero(mask)[0])
+        # Add new children to descendants
+        new = children - descendants
+        if not new:
+            break
+        descendants |= new
+        frontier = new
+
+    return descendants
+
+def get_non_robot_bodies(model, robot_root_name):
+    # Robot root ID
+    robot_root_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_BODY, robot_root_name)
+
+    # All robot bodies (subtree of robot root)
+    robot_bodies = get_body_descendants(model, robot_root_id)
+
+    # All bodies in scene
+    all_bodies = set(range(model.nbody))
+
+    # Everything else is "non-robot"
+    non_robot_bodies = all_bodies - robot_bodies
+
+    return non_robot_bodies
+
+def get_bodies_geoms(model, body_ids):
+    return np.concatenate([get_body_geoms(model, body_id) for body_id in body_ids])
+
+def get_body_geoms(model, body_id):
+    return np.arange(model.body_geomadr[body_id], model.body_geomadr[body_id] + model.body_geomnum[body_id])
